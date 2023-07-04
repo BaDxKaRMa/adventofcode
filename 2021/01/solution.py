7a09599d5683fea760f451703f0ac8072f1d7f10
BaDxKaRMa <bad87karma@gmail.com>
2023-07-04 15:12:31 -0400

    init commit

---
+++ 2021/01/solution.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+## advent of code 2021
+## https://adventofcode.com/2021
+## day 01
+
+import sys
+
+from loguru import logger
+
+logger.remove()
+logger.add(sys.stderr, level="INFO")
+
+def parse_input(lines):
+    pass
+
+def part1(data):
+    pass
+
+def part2(data):
+    pass
