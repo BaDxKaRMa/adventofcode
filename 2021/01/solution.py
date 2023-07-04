173ace7f38fecea56aa42a7dd8a91a7a5fb81cb8
BaDxKaRMa <bad87karma@gmail.com>
2023-07-04 16:15:07 -0400

    Finished day 1

--- 2021/01/solution.py
+++ 2021/01/solution.py
@@ -12,12 +12,21 @@ logger.add(sys.stderr, level="INFO")
 
 
 def parse_input(lines):
-    pass
+    return [int(i) for i in lines]
 
 
 def part1(data):
-    pass
+    answer = 0
+    prev = data[0]
+    for i in data[1:]:
+        if i > prev:
+            answer += 1
+        prev = i
+    return answer
 
 
 def part2(data):
-    pass
+    windows = [sum(i) for i in zip(data, data[1:], data[2:])]
+    pairs = zip(windows, windows[1:])
+    answer = [b - a for a, b in pairs if b - a > 0]
+    return len(answer)
