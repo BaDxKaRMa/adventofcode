5c20f2d38b43901016ab1489b11948358d90fd15
BaDxKaRMa <bad87karma@gmail.com>
2023-07-04 15:22:17 -0400

    Added example and formatted script to match standards. Updated template

--- 2021/01/solution.py
+++ 2021/01/solution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-## advent of code 2021
-## https://adventofcode.com/2021
-## day 01
+# advent of code 2021
+# https://adventofcode.com/2021
+# day 01
 
 import sys
 
@@ -10,11 +10,14 @@ from loguru import logger
 logger.remove()
 logger.add(sys.stderr, level="INFO")
 
+
 def parse_input(lines):
     pass
 
+
 def part1(data):
     pass
+
 
 def part2(data):
     pass
