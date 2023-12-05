3c80564b9763351ef2ef2f7ec1007a2850c15fc5
BaDxKaRMa <24795160+BaDxKaRMa@users.noreply.github.com>
2023-12-04 21:16:29 -0500

    Updated formating

--- 2023/04/solution.py
+++ 2023/04/solution.py
@@ -3,10 +3,11 @@
 # https://adventofcode.com/2023
 # day 04
 
+import os
 import sys
 import unittest
-from loguru import logger
 
+from loguru import logger
 
 logger.remove()
 logger.add(sys.stderr, level="INFO")
@@ -14,60 +15,44 @@ logger.add(sys.stderr, level="INFO")
 
 class TestParts(unittest.TestCase):
     def setUp(self):
-        self.input_data = parse_input(
-            [
-                "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-                "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-                "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-                "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-                "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-                "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
-            ]
-        )
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        input_file = os.path.join(current_dir, "example_input.txt")
+        with open(input_file, "r") as file:
+            lines = file.readlines()
+        self.input_data = parse_input(lines)
 
     def test_part1(self):
-        pass
         self.assertEqual(part1(self.input_data), 13)
 
     def test_part2(self):
-        pass
         self.assertEqual(part2(self.input_data), 30)
 
 
 def parse_input(lines):
     cards = {}
     for i, line in enumerate(lines, start=1):
-        winningSet = set()
-        cardSet = set()
-
         _, cardNumbers = line.split(":")
-        cardNumbers = cardNumbers.split(" | ")
-        winningSet = set(cardNumbers[0].split())
-        cardSet = set(cardNumbers[1].split())
+        winningSet, cardSet = [
+            set(numbers.split()) for numbers in cardNumbers.split(" | ")
+        ]
 
         cards[i] = (winningSet, cardSet)
     return cards
 
 
 def part1(data):
-    sum = 0
+    total_score = 0
 
     for _, (winningSet, cardSet) in data.items():
         matches = winningSet.intersection(cardSet)
+        cardScore = pow(2, len(matches) - 1) if matches else 0
+        total_score += cardScore
 
-        if len(matches) > 0:
-            cardScore = 1
-            for _ in range(len(matches) - 1):
-                cardScore *= 2
-        else:
-            cardScore = 0
-
-        sum += cardScore
-    return sum
+    return total_score
 
 
 def part2(data):
-    multipliers = {i: 1 for i in data.keys()}
+    multipliers = [1] * len(data)
 
     for index, (winningSet, cardSet) in data.items():
         matchCount = len(winningSet.intersection(cardSet))
@@ -75,10 +60,10 @@ def part2(data):
         logger.debug(f"matches: {matchCount}")
 
         for x in range(index + 1, index + 1 + matchCount):
-            if x in multipliers:
-                multipliers[x] += 1 * multipliers[index]
+            if x < len(multipliers):
+                multipliers[x] += multipliers[index]
 
-    return sum(multipliers.values())
+    return sum(multipliers)
 
 
 if __name__ == "__main__":
