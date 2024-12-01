58449864ebd3c74ab20f388053fd9a5707f38c4c
BaDxKaRMa <24795160+BaDxKaRMa@users.noreply.github.com>
2024-12-01 11:15:47 -0500

    hope these changes are ok

---
+++ 2023/18/solution.py
@@ -0,0 +1,16 @@
+## advent of code 2023
+## https://adventofcode.com/2023
+## day 18
+
+
+def parse_input(lines):
+    return lines
+
+
+def part1(data):
+    print(data)
+    pass
+
+
+def part2(data):
+    pass
---
+++ 2023/19/solution.py
@@ -0,0 +1,20 @@
+## advent of code 2023
+## https://adventofcode.com/2023
+## day 19
+
+from my_utils import setup_logging
+
+logger = setup_logging(debug=True)
+
+
+def parse_input(lines):
+    logger.debug("Parsing input into list")
+    return lines
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
